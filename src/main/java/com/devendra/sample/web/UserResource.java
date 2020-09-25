328893e58fdf019794b589aa4403f3334c4ce04a
devendra-kmr <devendrakumar.address@gmail.com>
2020-09-26 02:20:56 +0530

    modified

--- src/main/java/com/devendra/sample/web/UserResource.java
+++ src/main/java/com/devendra/sample/web/UserResource.java
@@ -2,20 +2,28 @@ package com.devendra.sample.web;
 
 
 import com.devendra.sample.domain.User;
+import com.devendra.sample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/")
 @RestController
 public class UserResource {
-
+    @Autowired
+    private UserService userService;
     @GetMapping("user/")
-    public User getUserDetails(){
-        User user = new User();
-        user.setFirstName("Devendra");
-        user.setLastName("Kumar");
-        return user;
+    public List<User> getUserDetails(){
+        return userService.getUserList();
     }
 
+
+    @PostMapping("user/")
+    public User getUserDetails(User user){
+        return userService.createUser(user);
+    }
 }
